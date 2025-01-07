7995049014b5d36a05dc8ca4856ed3c13aef418e
Yayo <yacoubbeltanivelle@gmail.com>
2025-01-07 17:26:40 +0100

    Ajout-Card-Fini

--- script.js
+++ script.js
@@ -6,27 +6,62 @@ window.addEventListener("DOMContentLoaded", () => {
   const addCardBtn = document.getElementById("addCardBtn");
   const searchInput = document.getElementById("searchInput");
   const sortByPriorityBtn = document.getElementById("sortByPriorityBtn");
+  const closeBtn = document.getElementsByClassName("close-btn")[0];
 
   // Éventuellement, on écoute les événements
-  addCardBtn.addEventListener("click", () => {
-    // ...
-  });
+  addCardBtn.onclick = function () {
+    cardModal.style.display = "block";
+  }
+
+  closeBtn.onclick = function () {
+    cardModal.style.display = "none";
+  }
+
+  window.onclick = function (event) {
+    if (event.target === cardModal) {
+      cardModal.style.display = "none";
+    }
+  }
+
+
+  cardForm.onsubmit = function (event) {
+    event.preventDefault();
+
+
+    const title = document.getElementById("title").value;
+    const content = document.getElementById("content").value;
+    const priority = document.getElementById("priority").value;
+    const columnToDo = document.querySelector('.column[data-status="todo"]');
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-priority', priority);
+
+
+    card.innerHTML = `
+        <h3>${title}</h3>
+        <p>${content}</p>
+    `;
+
+    columnToDo.appendChild(card);
+
+    cardModal.style.display = "none";
+
+    cardForm.reset();
+  }
 
   searchInput.addEventListener("input", () => {
-    const keyword = searchInput.value.toLowerCase(); 
-    const cards = document.querySelectorAll(".card"); 
+    const keyword = searchInput.value.toLowerCase();
+    const cards = document.querySelectorAll(".card");
     cards.forEach(card => {
-      const taskContent = card.querySelector("h3").textContent.toLowerCase(); 
+      const taskContent = card.querySelector("h3").textContent.toLowerCase();
       if (taskContent.includes(keyword)) {
-        card.style.display = ""; 
+        card.style.display = "";
       } else {
-        card.style.display = "none"; 
+        card.style.display = "none";
       }
     });
   });
-
-  //test 
-  //test
 
   sortByPriorityBtn.addEventListener("click", () => {
     // ...
