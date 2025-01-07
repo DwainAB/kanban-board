385e1d9525a0fcdb8a93b6899de40fb521320023
Vincent Lav <vincent00lav@gmail.com>
2025-01-07 16:09:15 +0100

    Initilized priority function

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   searchInput.addEventListener("input", () => {
-    const keyword = searchInput.value.toLowerCase(); 
-    const cards = document.querySelectorAll(".card"); 
-    cards.forEach(card => {
-      const taskContent = card.querySelector("h3").textContent.toLowerCase(); 
+    const keyword = searchInput.value.toLowerCase();
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+      const taskContent = card.querySelector("h3").textContent.toLowerCase();
       if (taskContent.includes(keyword)) {
-        card.style.display = ""; 
+        card.style.display = "";
       } else {
-        card.style.display = "none"; 
+        card.style.display = "none";
       }
     });
   });
 
   sortByPriorityBtn.addEventListener("click", () => {
-    // ...
+    const columns = document.querySelectorAll(".column");
   });
 });
