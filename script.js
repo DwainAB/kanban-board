b7584d4cb8e3ca338d0a12ddbfb825d70ad63382
Dwain YumCo <dwainyumco@Air-de-Dwain.lan>
2025-01-07 15:29:52 +0100

    function completed

--- script.js
+++ script.js
@@ -13,7 +13,16 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   searchInput.addEventListener("input", () => {
-    // ...
+    const keyword = searchInput.value.toLowerCase(); 
+    const cards = document.querySelectorAll(".card"); 
+    cards.forEach(card => {
+      const taskContent = card.querySelector("h3").textContent.toLowerCase(); 
+      if (taskContent.includes(keyword)) {
+        card.style.display = ""; 
+      } else {
+        card.style.display = "none"; 
+      }
+    });
   });
 
   sortByPriorityBtn.addEventListener("click", () => {
